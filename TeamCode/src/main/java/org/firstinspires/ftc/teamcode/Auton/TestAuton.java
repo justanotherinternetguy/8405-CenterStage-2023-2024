44995e20437a7c95d2fa59d7ddfccc42548b2bb1
david-man <86388290+david-man@users.noreply.github.com>
2023-10-12 17:24:53 -0400

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/TestAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/TestAuton.java
@@ -18,7 +18,7 @@ public class TestAuton extends LinearOpMode {
         Robot robot = new Robot(hardwareMap, gamepad1);
         Odometry odometry = new Odometry(hardwareMap, robot.drive.imu);
 //        Movement movement = new Movement(robot.drive, odometry, this::opModeIsActive, new PID.Config(.11, 0, 0), new PID.Config(0.05, 0, 0), 3, telemetry);
-        Movement movement = new Movement(robot.drive, odometry, this::opModeIsActive, new PID.Config(0.225, 0, 0.05), new PID.Config(0.065, 0, 0), 0.25, telemetry);
+        Movement movement = new Movement(robot.drive, odometry, this::opModeIsActive, new PID.Config(0.17, 0, 0.05), new PID.Config(0.065, 0, 0), 0.25, telemetry);
         waitForStart();
         odometry.reset();
         robot.drive.imu.resetYaw();
@@ -31,8 +31,8 @@ public class TestAuton extends LinearOpMode {
 //            movement.move(new Pose2d(0.25, 24 * 2.5, new Rotation2d(Math.toRadians(90))));
 //            movement.move(new Pose2d(24 * 0.75, 24 * 1.5, new Rotation2d(Math.toRadians(90))));
 
-            movement.move(new Pose2d(12, 24, new Rotation2d(Math.toRadians(0))));
-//            movement.move(new Pose2d(0, 48, new Rotation2d(Math.toRadians(0))));
+//            movement.move(new Pose2d(12, 24, new Rotation2d(Math.toRadians(0))));
+            movement.move(new Pose2d(0, 48, new Rotation2d(Math.toRadians(0))));
 //            movement.move(new Pose2d(12, 0, new Rotation2d(Math.toRadians(0))));
 //            movement.move(new Pose2d(0, 0, new Rotation2d(Math.toRadians(0))));
         }
