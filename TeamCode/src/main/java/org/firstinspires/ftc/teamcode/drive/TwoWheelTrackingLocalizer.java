d0eace7ee53f3d4a42c180903b3a8e8b8973ec29
david-man <86388290+david-man@users.noreply.github.com>
2023-10-16 11:04:21 -0400

    geniuses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -62,8 +62,8 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
 
         this.drive = drive;
 
-        parallelEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontRight"));
-        perpendicularEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backLeft"));
+        parallelEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backLeft"));
+        perpendicularEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontRight"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
     }
