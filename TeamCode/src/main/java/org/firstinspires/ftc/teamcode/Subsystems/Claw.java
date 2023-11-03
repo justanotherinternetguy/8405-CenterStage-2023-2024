0781cc1e97a53ffe9810a9a292abcf5267113cc3
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2023-11-03 17:29:24 -0400

    Update Claw.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
@@ -23,9 +23,18 @@ public class Claw {
         if (gamepad.right_bumper) {
             outtake();
         }
+        else {
+            servo1.setPower(0);
+            servo2.setPower(0);
+        }
     }
+
     private void intake() {
+        servo1.setPower(0.2);
+        servo2.setPower(-0.2);
     }
     private void outtake() {
+        servo1.setPower(-0.2);
+        servo2.setPower(0.2);
     }
 }
