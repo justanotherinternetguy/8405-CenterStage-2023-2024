a5a5cc66f4bb5256c677213124640a2a9db971c6
david-man <86388290+david-man@users.noreply.github.com>
2023-11-22 11:10:45 -0500

    formatting fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AUTON_RED_NEAR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AUTON_RED_NEAR.java
@@ -47,65 +47,65 @@ public class AUTON_RED_NEAR extends LinearOpMode {
             int centerX = objectCenter[0];
             if (centerX < third) { // left
                 direction = 0;
-                path.add(new MvntAction(pose2d(0, 28, 0)))                          // drive to tape
+                path.add(new MvntAction(pose2d(0, 28, 0)))                  // drive to tape
                         .add(new MvntAction(pose2d(-3, 28, -90)))           // face the tape
-                        .add(new ClawAction(ClawAction.ClawStates.bottomOpen))                                         // drop the purple pixel
+                        .add(new ClawAction(ClawAction.ClawStates.bottomOpen))              // drop the purple pixel
                         .add(new MvntAction(pose2d(0, 28, -90)))            // move back and up the claw
                         .add(new LiftAction(300, Config.liftMotorPowerAuton), true)
                         .add(new MvntAction(pose2d(0, 28, 90)))             // face the board
                         .add(new MvntAction(pose2d(Config.BACKSPOT, 34.5, 90))) // move to the board and raise lifts
                         .add(new LiftAction(Config.LIFT_BACK, Config.liftMotorPowerAuton * 1.1), true)
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 800)                     // drive into the wall
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 400)                     // continue driving into the wall and move lift down(to tilt the pixel)
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 800) // drive into the wall
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 400) // continue driving into the wall and move lift down(to tilt the pixel)
                         .add(new LiftAction(Config.LIFT_BACK - 100, Config.liftMotorPowerDown), true)
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 200)                     // continue driving and drop the yellow pixel
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 200) // continue driving and drop the yellow pixel
                         .add(new ClawAction(ClawAction.ClawStates.topOpen), true)
                         .add(new MvntAction(pose2d(24, 34.5, 90)))          // move back from the board and lower lift
                         .add(new LiftAction(Config.FLOOR + 40, Config.liftMotorPowerDown * 0.8), true)
-                        .add(new MvntAction(pose2d(24, 3, 90)))                     // move to the wall
-                        .add(new MvntAction(pose2d(45, 3, 90)));                    // park
+                        .add(new MvntAction(pose2d(24, 3, 90)))             // move to the wall
+                        .add(new MvntAction(pose2d(45, 3, 90)));            // park
             }
             else if (centerX > 2 * third) { // right
                 direction = 1;
                 path.add(new MvntAction(pose2d(0, 30, 0)))                 // drive to tape
-                        .add(new MvntAction(pose2d(3, 30, 90)))   // face tape
-                        .add(new ClawAction(ClawAction.ClawStates.bottomOpen))                               // drop purple pixel
-                        .add(new MvntAction(pose2d(0, 30, 90)))   // center and raise lift
+                        .add(new MvntAction(pose2d(3, 30, 90)))            // face tape
+                        .add(new ClawAction(ClawAction.ClawStates.bottomOpen))             // drop purple pixel
+                        .add(new MvntAction(pose2d(0, 30, 90)))            // center and raise lift
                         .add(new LiftAction(100, Config.liftMotorPowerAuton), true)
                         .add(new MvntAction(pose2d(12, 14, 90)))           // move back a tile and start raising lift
                         .add(new LiftAction(Config.LIFT_BACK, Config.liftMotorPowerAuton * 1.1), true, true)
                         .add(new MvntAction(pose2d(24, 14, 90)))           // move forward and continue raising lift
                         .add(new LiftAction(Config.LIFT_BACK, Config.liftMotorPowerAuton * 1.1), true, true)
-                        .add(new MvntAction(pose2d(Config.BACKSPOT, 22, 90))) // move to the board and continue raising lift
+                        .add(new MvntAction(pose2d(Config.BACKSPOT, 22, 90)))  // move to the board and continue raising lift
                         .add(new LiftAction(Config.LIFT_BACK, Config.liftMotorPowerAuton * 1.1), true, true)
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 800)           // drive into the wall
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 400)           // continue driving into the wall and move lift down(to tilt the pixel)
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 800) // drive into the wall
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 400) // continue driving into the wall and move lift down(to tilt the pixel)
                         .add(new LiftAction(Config.LIFT_BACK - 100, Config.liftMotorPowerDown), true)
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 200)           // continue driving and drop the yellow pixel
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 200) // continue driving and drop the yellow pixel
                         .add(new ClawAction(ClawAction.ClawStates.topOpen), true)
                         .add(new MvntAction(pose2d(24, 22, 90)))  // move back from the board and lower lift
                         .add(new LiftAction(Config.FLOOR + 40, Config.liftMotorPowerDown * 0.8), true)
-                        .add(new MvntAction(pose2d(24, 3, 90)))           // move to the wall
-                        .add(new MvntAction(pose2d(45, 3, 90)));          // park
+                        .add(new MvntAction(pose2d(24, 3, 90)))             // move to the wall
+                        .add(new MvntAction(pose2d(45, 3, 90)));            // park
             }
             else {
                 direction = 2;
-                path.add(new MvntAction(pose2d(0, 28, 0)))                            // move to tape
-                        .add(new ClawAction(ClawAction.ClawStates.bottomOpen))                                           // drop purple pixel
-                        .add(new LiftAction(300, Config.liftMotorPowerAuton))                                   // lift up height a bit while also moving back(to not knock over the purple pixel)
+                path.add(new MvntAction(pose2d(0, 28, 0)))                   // move to tape
+                        .add(new ClawAction(ClawAction.ClawStates.bottomOpen))               // drop purple pixel
+                        .add(new LiftAction(300, Config.liftMotorPowerAuton))        // lift up height a bit while also moving back(to not knock over the purple pixel)
                         .add(new MvntAction(pose2d(0, 26, 0)), true)
                         .add(new MvntAction(pose2d(0, 27.5, 90)))            // rotate towards the board
-                        .add(new MvntAction(pose2d(Config.BACKSPOT, 27.5, 90)))  // move to the board while moving the lift up
+                        .add(new MvntAction(pose2d(Config.BACKSPOT, 27.5, 90))) // move to the board while moving the lift up
                         .add(new LiftAction(Config.LIFT_BACK, Config.liftMotorPowerAuton * 1.1), true)
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 800)                      // drive into the wall
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 400)                      // continue driving into the wall and move lift down(to tilt the pixel)
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 800)  // drive into the wall
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 400)  // continue driving into the wall and move lift down(to tilt the pixel)
                         .add(new LiftAction(Config.LIFT_BACK - 100, Config.liftMotorPowerDown), true)
-                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 200)                      // continue driving and drop the yellow pixel
+                        .add(new MvntAction(Config.BOARDSPEED, 0, 0), 200)  // continue driving and drop the yellow pixel
                         .add(new ClawAction(ClawAction.ClawStates.topOpen), true)
                         .add(new MvntAction(pose2d(24, 27, 90)))             // move back from the board and lower lift
                         .add(new LiftAction(Config.FLOOR + 40, Config.liftMotorPowerDown * 0.8), true)
-                        .add(new MvntAction(pose2d(24, 3, 90)))                       // move to the wall
-                        .add(new MvntAction(pose2d(45, 3, 90)));                      // park into the slot
+                        .add(new MvntAction(pose2d(24, 3, 90)))              // move to the wall
+                        .add(new MvntAction(pose2d(45, 3, 90)));             // park into the slot
             }
         }
 
