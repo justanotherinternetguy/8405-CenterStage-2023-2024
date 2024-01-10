fcb5d4386b91a34f9b99a6f31ae5bcc367be8f8a
david-man <86388290+david-man@users.noreply.github.com>
2024-01-10 16:40:25 -0500

    Close red auto works!
    On paper. Need new claw pieces to test with pixels.
    TODO: add custom timeouts for each segment for left and center. Maybe also add a way to keep 0 stuff constant for the directional motor power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/ActorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/ActorTest.java
@@ -22,26 +22,67 @@ public class ActorTest extends LinearOpMode {
         Movement movement = new Movement(robot.drive);
         Actor actor = new Actor(hardwareMap, telemetry, robot, rrDrive, movement, 5000);
 
-//        while (!this.isStarted()) {
-//            if (gamepad1.x) {
-//                actor.add(new ClawAction(ClawAction.ClawStates.bottomClosed, ClawAction.ClawStates.topClosed), 100.0)
-//                        .add(new ClawAction(true), 500.0)
-//                        .add(new MvntAction(new Pose2d(0, 24, new Rotation2d(0))))
-//                        .add(new ClawAction(false), 500.0)
-//                        .add(new ClawAction(ClawAction.ClawStates.bottomOpen));
-        actor.add(new ClawAction(ClawAction.ClawStates.bottomClosed, ClawAction.ClawStates.topClosed), 2000.0)
-                .add(new ClawAction(true), 1000.0)
-                .add(new MvntAction(new Pose2d(0, 24, new Rotation2d(0))))
-                .add(new ClawAction(false), 1000.0)
-                .add(new ClawAction(ClawAction.ClawStates.bottomOpen), 1000.0)
-                .add(new ClawAction(true), 1000.0)
-                .add(new MvntAction(new Pose2d(0, 20, new Rotation2d(0))))
-                .add(new MvntAction(new Pose2d(32, 27, new Rotation2d(Math.toRadians(90)))))
-                .add(new LiftAction(500, Config.liftMotorPowerAuton))
-                .add(new MvntAction(0.25, 0.0, 0.0))
-                .add(new LiftAction(500, Config.liftMotorPowerAuton), true, true);
-//            }
-//        }
+        if (Config.dir == 0) {
+            actor.add(new ClawAction(ClawAction.ClawStates.bottomClosed, ClawAction.ClawStates.topClosed), 500.0)
+                    .add(new ClawAction(true), 750.0)
+                    .add(new MvntAction(new Pose2d(3, 27.5, new Rotation2d(Math.toRadians(-90)))))
+                    .add(new MvntAction(new Pose2d(-4.5, 27, new Rotation2d(Math.toRadians(-90)))))
+                    .add(new ClawAction(false), 750.0)
+                    .add(new ClawAction(ClawAction.ClawStates.bottomOpen), 1000.0)
+                    .add(new ClawAction(true), 750.0)
+                    .add(new MvntAction(new Pose2d(12, 32.5, new Rotation2d(Math.toRadians(-90)))))
+                    .add(new MvntAction(new Pose2d(24, 32.5, new Rotation2d(Math.toRadians(90)))))
+                    .add(new MvntAction(new Pose2d(32, 32.5, new Rotation2d(Math.toRadians(90)))))
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton))
+                    .add(new MvntAction(1/3.0, 0.0, 0.0), 1000.0)
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton), true, true)
+                    .add(new ClawAction(ClawAction.ClawStates.topOpen), 750.0)
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton), true, true)
+                    .add(new MvntAction(new Pose2d(32, 31, new Rotation2d(Math.toRadians(90)))))
+                    .add(new MvntAction(new Pose2d(32, 3, new Rotation2d(Math.toRadians(90)))))
+                    .add(new MvntAction(new Pose2d(45, 3, new Rotation2d(Math.toRadians(90)))));
+        }
+
+        if (Config.dir == 1) {
+            actor.add(new ClawAction(ClawAction.ClawStates.bottomClosed, ClawAction.ClawStates.topClosed), 500.0)
+                    .add(new ClawAction(true), 1000.0)
+                    .add(new MvntAction(new Pose2d(0, 27, new Rotation2d(0))))
+                    .add(new ClawAction(false), 1000.0)
+                    .add(new ClawAction(ClawAction.ClawStates.bottomOpen), 1000.0)
+                    .add(new ClawAction(true), 1000.0)
+                    .add(new MvntAction(new Pose2d(0, 20, new Rotation2d(0))))
+                    .add(new MvntAction(new Pose2d(32, 27, new Rotation2d(Math.toRadians(90)))))
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton))
+                    .add(new MvntAction(0.25, 0.0, 0.0))
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton), true, true)
+                    .add(new ClawAction(ClawAction.ClawStates.topOpen), 1000.0)
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton), true, true)
+                    .add(new MvntAction(new Pose2d(32, 27, new Rotation2d(Math.toRadians(90)))))
+                    .add(new MvntAction(new Pose2d(32, 3, new Rotation2d(Math.toRadians(90)))))
+                    .add(new MvntAction(new Pose2d(45, 3, new Rotation2d(Math.toRadians(90)))));
+        }
+
+        if (Config.dir == 2) {
+            actor.add(new ClawAction(ClawAction.ClawStates.bottomClosed, ClawAction.ClawStates.topClosed), 500.0)
+                    .add(new ClawAction(true), 750.0)
+                    .add(new MvntAction(new Pose2d(4, 27, new Rotation2d(Math.toRadians(90)))), 2500.0)
+                    .add(new ClawAction(false), 750.0)
+                    .add(new ClawAction(ClawAction.ClawStates.bottomOpen), 500.0)
+                    .add(new ClawAction(true), 750.0)
+                    .add(new MvntAction(new Pose2d(0, 27, new Rotation2d(Math.toRadians(90)))), 750.0)
+                    .add(new MvntAction(new Pose2d(0, 3, new Rotation2d(Math.toRadians(90)))), 2000.0)
+                    .add(new MvntAction(new Pose2d(32, 3, new Rotation2d(Math.toRadians(90)))), 2500.0)
+                    .add(new MvntAction(new Pose2d(32, 21.625, new Rotation2d(Math.toRadians(90)))), 2500.0)
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton))
+                    .add(new MvntAction(1/3.0, 0.0, 0.0), 1750.0)
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton), true, true)
+                    .add(new ClawAction(ClawAction.ClawStates.topOpen), 750.0)
+                    .add(new LiftAction(Config.boardBase + 100, Config.liftMotorPowerAuton), true, true)
+                    .add(new MvntAction(new Pose2d(32, 21.625, new Rotation2d(Math.toRadians(90)))))
+                    .add(new MvntAction(new Pose2d(32, 3, new Rotation2d(Math.toRadians(90)))), 2000.0)
+                    .add(new MvntAction(new Pose2d(45, 3, new Rotation2d(Math.toRadians(90)))), 2500.0);
+        }
+
 
         waitForStart();
 
